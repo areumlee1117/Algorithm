2d02d9936d586b7d35b2af61d0a9e62eccb922c6
arareum <147472852+areumlee1117@users.noreply.github.com>
2023-11-16 11:28:02 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,8 +1,13 @@
+// function solution(x) {
+//   let sum = 0;
+//   let arr = String(x).split("");
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += +(arr[i]);
+//   }
+//   return (x % sum == 0) ? true : false;
+// }
+
+
 function solution(x) {
-  let sum = 0;
-  let arr = String(x).split("");
-  for (let i = 0; i < arr.length; i++) {
-    sum += +(arr[i]);
-  }
-  return (x % sum == 0) ? true : false;
+  return !(x % (x + "").split("").reduce((a, b) => +a + +b));
 }
