3343bb2e463a97ddfb50960b7498a2cbcc880a4d
arareum <147472852+areumlee1117@users.noreply.github.com>
2023-11-16 10:02:59 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,8 +1,14 @@
-function solution(n) {
-  for (let x = 1; x * x <= n; x++) {
-    if (x * x === n) {
-      answer = (x + 1) * (x + 1);
-    } else answer = -1;
-  }
-  return answer;
+function solution(n) { 
+    
+    // i를 1부터 n까지 증가시켜줍니다.
+    for (let i = 1; i <= n; i++) {
+        // if문으로 i 와 i 를 곱했을 때 정확히 n이 되는 i 값을 구해줍니다.
+        if (i * i === n) {
+            // i 와 i 가 곱해졌을 때 n이 된다면 아래의 식을 실행하여 답을 제출
+            return (i + 1) * (i + 1);
+        }
+    }
+
+    // 그게 아니라면 -1을 제출
+    return -1;
 }
