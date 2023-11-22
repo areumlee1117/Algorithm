dff0ef4fd558f3b9fce3d69463cbdd5b6af7e309
arareum <147472852+areumlee1117@users.noreply.github.com>
2023-11-22 10:09:23 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,11 @@
 function solution(num) {
-
+  let answer = 0;
   for (let i = 0; i < 500; i++) {
-    if (num % 2 == 0) {
-      num = num / 2;
-    } else if (num == 1) {
-      return i;
-    } else if (num % 2 == 1) {
-      num = num * 3 + 1;
+    if (num != 1) {
+      num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+    } else {
+      return (answer = i);
     }
   }
-  return -1;
+  return (answer = -1);
 }
