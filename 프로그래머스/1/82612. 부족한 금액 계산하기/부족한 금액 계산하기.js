a138d0aa08064c7da5742b1e1dd86c0b8a373602
areumlee1117 <147472852+areumlee1117@users.noreply.github.com>
2023-12-21 09:54:15 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -5,3 +5,7 @@ function solution(price, money, count) {
   }
   return money > totalPrice ? 0 : totalPrice - money;
 }
+
+// 필요한 금액은 1*price + 2*price + ... + i*price
+// for문으로 i까지 돌린 후 필요한 금액 구하기
+// money와 필요한 금액 비교하기
